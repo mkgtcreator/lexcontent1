ecb7d18697305e7bb4ac23f09f1f1aaf936b6e0c
mkgtcreator <servicesbrava@gmail.com>
2025-12-19 20:45:43 -0300

    Refactor vite.config.ts to use fileURLToPath

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -21,4 +25,3 @@ export default defineConfig(({ mode }) => {
     }
   }
 })
-
